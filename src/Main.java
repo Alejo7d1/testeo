f5a5653d4d78f06ef0df741858fc33397f4fe82c
Alejo <alejo7d1@gmail.com>
2024-09-03 20:42:01 -0600

    cambios menores

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String mitexto = "Hello World!";
-        System.out.println("args = " + mitexto);
+        String mitexto = "el pan";
+        System.out.println("Me gusta" + mitexto);
     }
 }
