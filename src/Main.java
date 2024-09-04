d5b1c9ce6f29a08af967e521dcb021aee676923b
Alejo <alejo7d1@gmail.com>
2024-09-03 20:48:46 -0600

    otro comit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int mitexto = 11;
-        System.out.println("Me gusta " + mitexto);
+        System.out.println("No me gusta " + mitexto);
     }
 }
